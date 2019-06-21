b40845bfd29e5e8cab5a5be94c24deb34d7e18f8
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-06-21 15:06:08 -0400

    Updates

---
+++ precache-manifest.68fccf248a128ba48066c96992a337ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe90f6c6eb7328269af4a498ec82501e",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "f9e8466ce3c30fb9a71a",
+    "url": "/click-game/static/css/main.6c8875dc.chunk.css"
+  },
+  {
+    "revision": "d2608c4f6ab882cf92ea",
+    "url": "/click-game/static/js/2.fc68dfc8.chunk.js"
+  },
+  {
+    "revision": "f9e8466ce3c30fb9a71a",
+    "url": "/click-game/static/js/main.c8f82ca7.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
